0ee9d464589a3014ed7d7c0b1d124fab6e3de8c5
Federico DApote <Federico.DAPote@weaveability.com>
2019-11-11 14:56:10 +0000

    Updates

---
+++ precache-manifest.5b80029162ff272e4447d5aafd7373c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4843a26d7f57ff7995f3f6b39201e7f9",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "e65cb9eeed694fea7c19",
+    "url": "/crwn-clothing/static/css/main.0211b73b.chunk.css"
+  },
+  {
+    "revision": "3cc94ec080955ce0c256",
+    "url": "/crwn-clothing/static/js/2.be3c6314.chunk.js"
+  },
+  {
+    "revision": "e65cb9eeed694fea7c19",
+    "url": "/crwn-clothing/static/js/main.3b91c4d2.chunk.js"
+  },
+  {
+    "revision": "c6ff65db4cbf77ead457",
+    "url": "/crwn-clothing/static/js/runtime-main.e22ca938.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
